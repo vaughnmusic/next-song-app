6fe03374bffcaadb2582ee529cd6f064232cd00e
Eric <110043489+vaughnmusic@users.noreply.github.com>
2022-09-29 12:37:44 -0400

    performer can view song reqeuest for a gig

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -66,4 +66,21 @@ export function submitNewRequest(gigId, songId) {
     return axios.post(`${NEXT_SONG_URL}/requests`, { gigId, songId });
 };
 
+export function getSongRequests(gigId) {
+    // get requests from our api '/api/requests/:gigId'
+    return axios.get(`${NEXT_SONG_URL}/requests/${gigId}`)
+}
 
+/**
+ * 
+ * @param {string[]} songIds array of spotify song ids
+ * @returns 
+ */
+export function getTracks(songIds) {
+    return axios.get(`${SPOTIFY_URL}/tracks`, {
+        headers: { Authorization: `Bearer ${getToken()}` },
+        params: { 
+            ids: songIds.join()
+        }
+    });
+}
